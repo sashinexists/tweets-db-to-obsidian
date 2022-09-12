67fd0f63571d85dbe8a6ff9dd88b91178d5ce1f8
sashiexists <myself@sashinexists.com>
2022-09-13 09:37:36 +1000

    add twitter handle to user

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use data::entities::users;
 use futures::executor::block_on;
 use utils::TweetData;
 
@@ -10,34 +11,70 @@ fn main() {
     let db = block_on(data::setup::set_up_db()).expect("Failed to load database");
     let users: Vec<UserData> = block_on(data::read::users(&db));
     let tweets: Vec<TweetData> = block_on(data::read::tweets(&db));
-//    users.iter().for_each(|user| println!("{:?}", user.user));
-    
     create_dirs();
     tweets.into_iter().for_each(|tweet| {
-        write_tweet(tweet);
+        write_tweet(tweet, &users);
     });
 
     
 }
 
-fn write_tweet(tweet_data: TweetData) {
-    let formatted_tweet = format_tweet(&tweet_data);
-    let tweet = tweet_data.tweet.expect("Invalid tweet");
+fn write_tweet(tweet_data: TweetData, users: &[UserData]) {
+    let tweet = tweet_data.clone().tweet.expect("Invalid tweet");
+    let formatted_tweet = format_tweet(&tweet_data, users);
     let path = format!("tweet-vault/tweets/{}.md", tweet.id);
     fs::write(path, formatted_tweet).expect("Unable to write file");
 }
 
-fn format_tweet(tweet_data: &TweetData)-> String {
+fn format_tweet(tweet_data: &TweetData, users: &[UserData])-> String {
     let tweet = tweet_data.clone().tweet.expect("Invalid tweet");
+    let user = users.iter().find(|user| user.user.clone().expect("Invalid user").id == tweet.author_id).expect("Invalid user").user.clone().unwrap();
+    let author_twitter_handle = user.username;
     let tweet_template = fs::read_to_string("templates/Tweet.md").expect("Failed to read tweet template");
         tweet_template
             .replace("{{CONTENT}}", &tweet.content)
             .replace("{{TWEET_ID}}", &tweet.id.to_string())
-            .replace("{{AUTHOR_TWITTER_HANDLE}}", &tweet.author_id.to_string())
+            .replace("{{AUTHOR_TWITTER_HANDLE}}", &format!("[[@{}]]",&author_twitter_handle))
             .replace("{{PUBLISHED_DATE}}", &tweet.created_at.to_string())
-            .replace("{{CONVERSATION_ID}}", &tweet.conversation_id.to_string())
+            .replace("{{CONVERSATION_ID}}", &format!("[[Conversation-{}]]",&tweet.conversation_id))
+            .replace("{{IN_REPLY_TO_ID}}", &format_in_reply_to(tweet_data))
+            .replace("{{RETWEET_ID}}", &format_retweeted(tweet_data))
+            .replace("{{QUOTED_TWEET_ID}}", &format_quoted(tweet_data))
 }
 
+fn format_in_reply_to(tweet_data: &TweetData) -> String {
+    let references = tweet_data.clone().references;
+    let reply_to = references.into_iter().find(|reference| reference.reference_type == "replied_to");
+    match reply_to {
+        Some(reply_to) => {
+            format!("[[{}]]", reply_to.referenced_tweet_id)
+        },
+        None => String::from("None"),
+    }
+}
+
+
+fn format_retweeted(tweet_data: &TweetData) -> String {
+    let references = tweet_data.clone().references;
+    let reply_to = references.into_iter().find(|reference| reference.reference_type == "retweeted");
+    match reply_to {
+        Some(reply_to) => {
+            format!("[[{}]]", reply_to.referenced_tweet_id)
+        },
+        None => String::from("None"),
+    }
+}
+
+fn format_quoted(tweet_data: &TweetData) -> String {
+    let references = tweet_data.clone().references;
+    let reply_to = references.into_iter().find(|reference| reference.reference_type == "quoted");
+    match reply_to {
+        Some(reply_to) => {
+            format!("[[{}]]", reply_to.referenced_tweet_id)
+        },
+        None => String::from("None"),
+    }
+}
 fn create_dirs() {
     match fs::create_dir("tweet-vault") {
         Ok(_) => println!("Created tweet-vault directory"),
--- templates/Tweet.md
+++ templates/Tweet.md
@@ -3,5 +3,8 @@ id: {{TWEET_ID}}
 author: {{AUTHOR_TWITTER_HANDLE}}
 published_date: {{PUBLISHED_DATE}}
 conversation_id: {{CONVERSATION_ID}}
+in_reply_to: {{IN_REPLY_TO_ID}}
+retweet: {{RETWEET_ID}}
+quoted_tweet: {{QUOTED_TWEET_ID}}
 ---
 {{CONTENT}}
